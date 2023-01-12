3ec79246892c90897facb9d1cd9fc1ffbda3e4f5
theclubfoot <papakwesiadams@icloud.com>
2023-01-12 23:34:00 +0000

    country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+-- script that creates a table users following these requirements
+-- id, email, name, and country
+-- by phils
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
