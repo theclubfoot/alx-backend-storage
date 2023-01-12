e009284a52207020048cf0cd870313019ef025e9
theclubfoot <papakwesiadams@icloud.com>
2023-01-12 23:43:04 +0000

    creates an index

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,5 @@
+-- script that creates an index idx_name_first_score on the
+-- table names and the first letter of name and the score
+-- by phils
+
+CREATE INDEX idx_name_first_score on names(name(1), score);
